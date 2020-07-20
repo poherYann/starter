b89fd02962332f37d2c03bf63194d9104ec17254
Yann Poher <yan.poher@gmail.com>
2020-07-20 13:02:10 +0200

    mise en place du starter

---
+++ inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Bootstrap doit être lancé avant tout autre fichier css-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <title>STARTER</title>
+
+</head>
+
+<body>
+<header>
+</header>
